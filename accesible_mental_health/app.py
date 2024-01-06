4a54a9ac9e0efd3cc30f3c8d560d70e4d41437a3
Pranav Gupta <pranav.gupta7315@gmail.com>
2024-01-06 13:27:12 -0700

    sup

--- accesible_mental_health/app.py
+++ accesible_mental_health/app.py
@@ -13,5 +13,34 @@ def providers():
 def appointments():
     return render_template('appointments.html')
 
+
+# code for graph
+y_values = []
+
+@app.route('/update_graph', methods=['POST'])
+def update_graph():
+    # Get input data from the request
+    data = request.get_json()
+    node1 = data.get('node1')
+    node2 = data.get('node2')
+
+    # Convert the input values to numbers and add them to the y-values list
+    y_values.extend([float(node1), float(node2)])
+
+    # Generate x-values at fixed intervals
+    x_values = list(range(len(y_values)))
+
+    # Return the new graph data
+    return jsonify({
+        'labels': x_values,
+        'datasets': [{
+            'label': 'My Graph',
+            'data': y_values,
+            'fill': False,
+            'borderColor': 'rgb(75, 192, 192)',
+            'tension': 0.1
+        }]
+    })
+
 if __name__ == '__main__':
     app.run(debug=True)
