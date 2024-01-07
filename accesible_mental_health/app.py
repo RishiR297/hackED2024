7a23c3bda84ef46a5ea6916b429e016c46aa81f1
Ash0106 <ashwinshanmugam.com>
2024-01-07 10:26:44 -0700

    gayyyyy

--- accesible_mental_health/app.py
+++ accesible_mental_health/app.py
@@ -16,7 +16,7 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 CORS(app)
 
 @login_manager.user_loader
@@ -117,6 +117,10 @@ def signup():
 @app.route('/newhome')
 def newhome():
     return render_template('newhome.html')
+
+@app.route('/calendar', methods = ["GET", "POST"])
+def calendar():
+    return render_template('calendar.html')
 
 @app.route('/reset_password')
 def reset_password():
